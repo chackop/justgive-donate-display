0a8d4f21320754f4ff629c5f114be8dac87c8312
Chacko <chackop@gmail.com>
2018-04-22 19:50:41 +0100

    material ui todo

--- src/components/DataService.js
+++ src/components/DataService.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+
 const APIURL = 'https://api.justgiving.com/0b324596/v1/charity/';
 const CHARID_BRITF = 183092;
 // const CHARID_MACSUPP = 2116;
@@ -70,6 +72,25 @@ class DataService extends Component {
                 <p>{this.state.profilePageUrl}</p>
                 <p>{this.state.registrationNumber}</p>
             </div>
+            // <Card>
+            //     <CardHeader
+            //         title="URL Avatar"
+            //         subtitle="Subtitle"
+            //         avatar="images/jsa-128.jpg"
+            //     />
+            //     <CardMedia
+            //         overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+            //     >
+            //         <img src="images/nature-600-337.jpg" alt="" />
+            //     </CardMedia>
+            //     <CardTitle title="Card title" subtitle="Card subtitle" />
+            //     <CardText>
+            //         Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            //         Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            //         Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            //         Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            //     </CardText>
+            // </Card>
         );
     }
 }
